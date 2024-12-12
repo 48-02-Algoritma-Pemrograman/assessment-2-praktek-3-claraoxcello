f8a4537751a68b08708fd32130fc75e1cd4d4ae8
claraoxcello <clarissa051205@gmail.com>
2024-12-12 14:31:40 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,25 @@
+
 import java.util.Scanner;
 
-public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+public class soalsatu {
+    static String fizz(int n) {
+        if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } 
+        return String.valueOf(n);
+        
     }
-    
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i =0;i<=n;i++){
+            System.out.println(fizz(i));
+        }
+        sc.close();
+    }
 }
